fc758863fb4e7a6752842893bdc0725787b861f5
martakortslaur <martakortslaur@gmail.com>
2019-11-07 12:29:41 +0000

    jee

---
+++ index.js
@@ -0,0 +1,8 @@
+$("#clicktoblue").click(function() {
+        $("#clicktoblue").addClass("blue");
+
+$("#alert").click(function() {
+    // $("#alert").addClass("window.alert");
+    alert("I am an alert box!");
+});
+});
